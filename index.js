05fb856fd7817c53bfd221a87b5a7e062f0f52a5
Joseph Stevens <josephscottstevens@gmail.com>
2019-09-23 14:52:07 -0400

    t

--- index.js
+++ index.js
@@ -21,13 +21,32 @@ function readJson(res, fileName) {
         res.send("error");
     })
     .fromFile(fileName)
-    .then(function(data){ //when parse finished, result will be emitted here.
+    .then(function(data) {
         console.log(data);
         res.send(data);
     });
 }
 
 var jsonParser = json();
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 app.post('/clockin', jsonParser, function (req, res) {
     console.log(req.body);
@@ -61,6 +80,20 @@ app.get('/jobroles/', function (req, res) {
 
 app.get('/employees', function (req, res) {
     readJson(res, 'data/employees.csv');
+});
+
+app.get('/timeentrystatus', function (req, res) {
+    csv()
+    .on('error',(err)=>{
+        console.log(err)
+        res.send("error");
+    })
+    .fromFile('data/clockin.csv')
+    .then(function(data) {
+        console.log(data);
+        Math.max(data)
+        res.send(data);
+    });
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
