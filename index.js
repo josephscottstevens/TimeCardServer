9409b964828a6202ae43b91b0ed96fa07125b71f
Joseph Stevens <josephscottstevens@gmail.com>
2019-11-27 11:52:16 -0500

    bug fix for access control removed un-needed log file

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const clockedin = "clockedin";
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8000');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
